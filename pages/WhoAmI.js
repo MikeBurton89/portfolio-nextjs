5644672668571c6282b6606c41e3e4b7d5e8f943
Michele Puricelli <michele.puricelli89@gmail.com>
2022-04-23 23:04:23 +0200

    added basic who am I page

---
+++ pages/WhoAmI.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LayoutContainer from '../components/UI/LayoutContainer'
+
+const WhoAmI = () => {
+    return (
+        <LayoutContainer>
+            <main>
+                <h1>Hi, my name is Michele but you can call me Mike.</h1>
+                <p>I am a web developer</p>
+            </main>
+        </LayoutContainer>
+    )
+}
+
+export default WhoAmI
