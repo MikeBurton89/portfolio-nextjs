9c4549e731178ba95918e52ea52f11feeda4d391
Michele Puricelli <michele.puricelli89@gmail.com>
2022-04-24 10:48:34 +0200

    added animations with react-reveal library

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Project from '../components/Project'
 import LayoutContainer from '../components/UI/LayoutContainer'
+import { Fade } from 'react-reveal'
 
 const Projects = () => {
     return (
         <>
             <LayoutContainer>
-                <Project />
-                <Project />
-                <Project />
-                <Project />
+                <Fade left><Project /></Fade>
+                <Fade right><Project /></Fade>
+                <Fade left><Project /></Fade>
+                <Fade right><Project /></Fade>
             </LayoutContainer>
 
         </>
