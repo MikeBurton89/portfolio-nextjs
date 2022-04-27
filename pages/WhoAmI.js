911ec22e274ea46a80f3fcec042ad2f21cc8b7b5
Michele Puricelli <40004867+MikeBurton89@users.noreply.github.com>
2022-04-27 17:08:58 +0200

    Update WhoAmI.js

--- pages/WhoAmI.js
+++ pages/WhoAmI.js
@@ -13,11 +13,11 @@ const WhoAmI = () => {
             </LayoutCard>
             <LayoutCard>
                 <p className='px-6'>
-                    {'This is a fine question indeed.
+                    {"This is a fine question indeed.
                     Let's just ignore the philosophy of the question and try to answer at least with basic info.
                     I'm italian, I'm {currentYear - 1989} years old
                     I am a web developer, I use computer to do things that you can click or touch on the internet.
-                    For example my age above will always be up to date thanks to something called Javascript.'}
+                    For example my age above will always be up to date thanks to something called Javascript."}
                 </p>
             </LayoutCard>
         </LayoutContainer>
